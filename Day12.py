5ae1d800edae97619a192ad57572bd96f1b7881b
HeSer <B@nd@r1359>
2023-01-18 08:58:53 -0500

    first update

--- Day12.py
+++ Day12.py
@@ -1,22 +1,70 @@
-# This is the start of the 12 day.
-#Stay focused and keep working.
-enemies = 1
-def increase_enemies():
-    global enemies # this will bring the global variable inside the local environment
-    enemies += 1 # try to avoid modifying global scope
-    print(f"enemies inside function {enemies}")
+def nr_attempts():
+    difficulty = input("please choose a difficulty level! hard or easy? ").lower()
+    if difficulty == "hard":
+        max_attempt = 5
+        print(f"You have {max_attempt} to complete the game")
+    else:
+        max_attempt = 10
+        print(f"you have {max_attempt} to complete the game")
+    return max_attempt
 #%%
-increase_enemies()
+# print(nr_attempts())
 #%%
-print(f"enemies from outside the program {enemies}")
+def check_answer(guess, answer, max_attempt):
+    """check answer against guess, returns the remaining attempts"""
+    if guess > answer:
+        print("too high")
+        return max_attempt - 1
+    elif guess == answer:
+        print("you won")
+    else:
+        print("too low")
+        return max_attempt - 1
+#%%
+#%%
+#%%
+#%%
+# print(nr_attempts())
+#%%
+#%%
+#%%
+
+from attempts import nr_attempts
+# from guess import yr_guess
+# from check_answer import check_answer
+#%%
+#%%
+def game():
+    print("welcome to the guess number!")
+    # nr_attempts()
+    max_attempts = nr_attempts()
+    print("I am thinking of a number between 1 and 100!")
+    import random
+    answer = random.randint(1, 100)
+    print(answer)
+    guess = 0
+    print(guess)
+    
+    while guess != answer:
+        print(f"you have {max_attempts} attempts left.")
+        guess = int(input("guess a number: "))
+        max_attempts = check_answer(guess, answer, max_attempts)
+        if max_attempts == 0:
+            print("game over! You run out of guess. You lose!")
+            break
+        elif guess != answer:
+            print("Guess again!")
+        
+        
+    
+#%%
+game()
+
 #%%
 
 
 #%%
-#%%
-#%%
-#%%
-#%%
-#%%
+
+
 #%%
 #%%
